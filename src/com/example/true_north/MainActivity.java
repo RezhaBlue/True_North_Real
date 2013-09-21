f146fc0aab9488ef3e194cea1cd44eb9751cd554
Bcom <Kj1646@gmail.com>
2013-09-21 05:40:18 -0400

    Second

--- src/com/example/true_north/MainActivity.java
+++ src/com/example/true_north/MainActivity.java
@@ -20,5 +20,39 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
 
 }
