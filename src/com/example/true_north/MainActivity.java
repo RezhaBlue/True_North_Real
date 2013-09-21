aab73898c492847663ffb7efcf7a856757ba2926
RezhaBlue <xiriox@gmail.com>
2013-09-21 08:43:28 -0400

    Update MainActivity.java
    Functioning talk with cloud and push notification from cloud

--- src/com/example/true_north/MainActivity.java
+++ src/com/example/true_north/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.true_north;
 
-import com.parse.Parse;
-import com.parse.ParseAnalytics;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseInstallation;
+//import com.parse.ParseObject;
+import com.parse.PushService;
+
 public class MainActivity extends Activity {
 
-	//This is called when the applications menu icon is first called.
+	//This is called when the application menu icon is first called
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Parse.initialize(this, "zfwXcHqiuDIkGFDXCVZhlr1vkNG2PxSbifpMKwhh", "TNV41MwKCQRAsF9qZ8LJuFEZD3sz2P6b81ecfENH");
+		
+		//specifies a Activity to be used for all push notifications by default
+		PushService.setDefaultPushCallback(this, MainActivity.class);
+		ParseInstallation.getCurrentInstallation().saveInBackground();
+		
+		//tracks statistics around application opens
 		ParseAnalytics.trackAppOpened(getIntent());
+		/**ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("foo", "bar");
+		testObject.saveInBackground();
+		*/
 	}
 
 	//This is called when the application is called. The Activity super object calls this by default.
